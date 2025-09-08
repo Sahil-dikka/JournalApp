91c3c96e1ee546d1f8ffa4a0b6394b8225654be6
Sahil Dikka <sahildikka2001@gmail.com>
2025-09-09 00:14:19 +0530

    api routes added

---
+++ frontend/src/ApiRoutes/ApiRoutes.js
@@ -0,0 +1,16 @@
+const ApiRoutes = {
+    GET : {
+        GET_USER : "/user",
+        GET_JOURNALS : "/journal"
+    },
+    POST : {
+        LOGIN :"/public/login",
+        REGISTER : "/public/signup",
+        CREATE_NEW_JOURNAL : "/journal"
+    },
+    PUT : {},
+
+    DELETE : {}
+};
+
+export default ApiRoutes;
