fd0186837a16d0eb148e91b3d2514a4300050e13
Sahil Dikka <sahildikka2001@gmail.com>
2025-09-16 20:53:57 +0530

    backend updated

--- backend/src/main/java/net/sd/journalApp/controller/JournalController.java
+++ backend/src/main/java/net/sd/journalApp/controller/JournalController.java
@@ -15,7 +15,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/journal")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class JournalController {
 
 
--- backend/src/main/java/net/sd/journalApp/controller/PublicController.java
+++ backend/src/main/java/net/sd/journalApp/controller/PublicController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/public")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @Slf4j
 public class PublicController {
